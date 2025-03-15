2e8d926a147a5225c8674bd7007d2a3ea8d2e89d
Seva <20211206nch@mail.ru>
2025-03-15 18:01:49 +0300

    index.js change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { thunk } from 'redux-thunk';
-import { configureStore, compose, applyMiddleware} from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import { rootReducer } from './redux/rootReducer';
 import { Provider } from 'react-redux';
 import { spamFilter } from './redux/middleware';
@@ -11,8 +11,8 @@ import App from './App';
 
   const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, spamFilter), // Исправлено
-    devTools: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Исправлено
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, spamFilter), 
+    devTools: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
 });
 
 ReactDOM.render(
